bbf8491808312570138a208538d20c8a29758094
oscar marina <oscar7600@outlook.com>
2024-12-11 14:03:15 +0100

    chore: bump vitest 3.0.0-beta.2

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
-/// <reference types="vitest/config" />
-
 import {defineConfig} from 'vite';
-import {rollupPluginHTML as pluginHtml} from '@web/rollup-plugin-html';
+import copy from 'rollup-plugin-copy';
+import multi from '@rollup/plugin-multi-entry';
 import totalBundlesize from '@blockquote/rollup-plugin-total-bundlesize';
 import externalizeSourceDependencies from '@blockquote/rollup-plugin-externalize-source-dependencies';
+
+const copyConfig = {
+  targets: [
+    {
+      src: 'demo/*.html',
+      dest: 'dev/',
+    },
+  ],
+  hook: 'writeBundle',
+};
 
 const outDir = process.env.OUTDIR || '.';
 
@@ -51,13 +60,16 @@ export default defineConfig({
        * @web/dev-server. So it should be ignored by Vite */
       '/__web-dev-server__web-socket.js',
     ]),
-    pluginHtml({input: 'demo/*.html'}),
+    copy(copyConfig),
+    multi({entryFileName: 'entry.js'}),
     totalBundlesize(),
   ],
   build: {
     target: ['chrome71'],
     outDir: 'dev',
     rollupOptions: {
+      preserveEntrySignatures: 'exports-only',
+      input: ['demo/*.js'],
       output: {
         dir: 'dev/',
         format: 'es',
