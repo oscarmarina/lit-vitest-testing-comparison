2f88e03300c2057701f5f8bf7531421c7732a000
oscar marina <oscar7600@outlook.com>
2024-12-11 14:26:50 +0100

    chore: bump vitest 3.0.0-beta.2

---
+++ demo/entry.js
@@ -0,0 +1,3 @@
+export {html, css, render} from 'lit';
+
+import '../src/define/counter-element.ts';
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 /// <reference types="vitest/config" />
 
 import {defineConfig} from 'vite';
-import {rollupPluginHTML as pluginHtml} from '@web/rollup-plugin-html';
+import copy from 'rollup-plugin-copy';
+import multi from '@rollup/plugin-multi-entry';
 import totalBundlesize from '@blockquote/rollup-plugin-total-bundlesize';
 import externalizeSourceDependencies from '@blockquote/rollup-plugin-externalize-source-dependencies';
+
+const copyConfig = {
+  targets: [
+    {
+      src: 'demo/*.html',
+      dest: 'dev/',
+    },
+  ],
+  hook: 'writeBundle',
+};
 
 const outDir = process.env.OUTDIR || '.';
 
@@ -28,7 +39,7 @@ export default defineConfig({
         functions: 80,
         lines: 80,
       },
-      include: ['**/src/**/*'],
+      include: ['src/**/*'],
       exclude: ['**/src/**/index.*', '**/src/styles/'],
     },
     browser: {
@@ -51,13 +62,19 @@ export default defineConfig({
        * @web/dev-server. So it should be ignored by Vite */
       '/__web-dev-server__web-socket.js',
     ]),
-    pluginHtml({input: 'demo/*.html'}),
+    copy(copyConfig),
+    multi({entryFileName: 'entry.js'}),
     totalBundlesize(),
   ],
+  optimizeDeps: {
+    exclude: ['lit', 'lit-html'],
+  },
   build: {
     target: ['chrome71'],
     outDir: 'dev',
     rollupOptions: {
+      preserveEntrySignatures: 'exports-only',
+      input: ['demo/*.js'],
       output: {
         dir: 'dev/',
         format: 'es',
